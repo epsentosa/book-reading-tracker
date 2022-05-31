8a3b6b929e777b52f2f80add48fb739612f10b81
eps-project <oke.ps4105@gmail.com>
2022-05-31 14:57:34 +0700

    register config #1

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
-from flask import Flask, redirect,render_template, url_for
+from flask import Flask, redirect,render_template, url_for, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app._static_folder = "static"
+
+app.config['MYSQL_HOST'] = '0.0.0.0'
+app.config['MYSQL_USER'] = 'eps'
+app.config['MYSQL_PASSWORD'] = 'brew'
+app.config['MYSQL_DB'] = 'track_books'
+
+mysql = MySQL(app)
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template("home.html")
 
-@app.route('/register')
+@app.route('/register',methods = ['POST','GET'])
 def register_page():
-    return render_template("register.html") 
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        with mysql.connection as conn:
+            cursor = conn.cursor()
+            insert_query = "INSERT INTO members (full_name,email,password) VALUES (%s,%s,%s);"
+            cursor.execute(insert_query,(name,email,password))
+            conn.commit()
+            return redirect('/home')
 
-@app.route('/login')
+    return render_template("register.html")
+
+@app.route('/login',methods = ['POST','GET'])
 def login_page():
     return render_template("login.html") 
 
