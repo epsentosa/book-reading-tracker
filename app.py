c87874f74fa7975fbf7200ecdc35871e1171ab4b
eps-project <oke.ps4105@gmail.com>
2022-06-02 15:27:41 +0700

    add hashing password

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, redirect,render_template, url_for, request
+from flask import Flask, redirect,render_template, url_for, request, flash
 from flask_mysqldb import MySQL
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app._static_folder = "static"
+bcrypt = Bcrypt(app)
 
 app.config['MYSQL_HOST'] = '0.0.0.0'
 app.config['MYSQL_USER'] = 'eps'
@@ -18,16 +20,18 @@ def home_page():
 
 @app.route('/register',methods = ['POST','GET'])
 def register_page():
+    error = None
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
-        with mysql.connection as conn:
-            cursor = conn.cursor()
+        psswd_hash = bcrypt.generate_password_hash(password)
+        with mysql.connection.cursor() as cursor:
             insert_query = "INSERT INTO members (full_name,email,password) VALUES (%s,%s,%s);"
-            cursor.execute(insert_query,(name,email,password))
-            conn.commit()
-            return redirect('/home')
+            cursor.execute(insert_query,(name,email,psswd_hash))
+            mysql.connection.commit()
+
+        return redirect(url_for('home_page'))
 
     return render_template("register.html")
 
