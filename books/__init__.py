2afb9970ad097dd3ddabe2b8a4cb26ac37bf1cbf
eps-project <105858684+eps-project@users.noreply.github.com>
2022-06-03 15:33:00 +0700

    modify login-session (#8)

--- books/__init__.py
+++ books/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, redirect,render_template, url_for, request, flash
+from unicodedata import category
+from flask import Flask, redirect,render_template, url_for, request, flash, session
 from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
-from books.forms import RegistrationForm
+from books.forms import RegistrationForm,LoginForm
 
 app = Flask(__name__)
 app._static_folder = "static"
@@ -18,10 +19,21 @@ mysql = MySQL(app)
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template("home.html")
+    msg = 'Welcome'
+    name = ''
+    is_auth = False
+    if "user" in session:
+        is_auth = True
+        msg = f"Succesfully Login for user {session['user']}"
+        name = session['user']
+
+    return render_template("home.html",msg=msg,auth=is_auth,name=name)
 
 @app.route('/register',methods = ['POST','GET'])
 def register_page():
+    if "user" in session:
+        return redirect(url_for('home_page'))
+
     form = RegistrationForm()
     if form.validate_on_submit():
         name = form.name.data
@@ -29,11 +41,19 @@ def register_page():
         password = form.password.data
         psswd_hash = bcrypt.generate_password_hash(password)
         with mysql.connection.cursor() as cursor:
+            email_check = "SELECT email FROM members WHERE email = %s;"
             insert_query = "INSERT INTO members (full_name,email,password) VALUES (%s,%s,%s);"
-            cursor.execute(insert_query,(name,email,psswd_hash))
-            mysql.connection.commit()
+            cursor.execute(email_check,(email,))
+            is_registered = cursor.fetchone()
+            if is_registered:
+                flash("Email already registered.")
+                return render_template("register.html",form=form)
+            else:
+                cursor.execute(insert_query,(name,email,psswd_hash))
+                mysql.connection.commit()
 
-        return redirect(url_for('home_page'))
+        flash("Registered Success, please Login.",category='success')
+        return redirect(url_for('login_page'))
     
     if form.errors: 
         for error in form.errors.values():
@@ -43,4 +63,36 @@ def register_page():
 
 @app.route('/login',methods = ['POST','GET'])
 def login_page():
-    return render_template("login.html") 
+    if "user" in session:
+        return redirect(url_for('home_page'))
+
+    form = LoginForm()
+    if request.method == "POST":
+        email = form.email.data
+        password = form.password.data
+        with mysql.connection.cursor() as cursor:
+            email_check = "SELECT email FROM members WHERE email = %s;"
+            password_check = "SELECT password FROM members WHERE email = %s;"
+            name = "SELECT full_name FROM members WHERE email = %s;"
+            cursor.execute(email_check,(email,))
+            is_email = cursor.fetchone()
+            if is_email:
+                cursor.execute(password_check,(email,))
+                password_check = cursor.fetchone()[0]
+                is_password = bcrypt.check_password_hash(password_check,password)
+                if is_password:
+                    cursor.execute(name,(email,))
+                    name = cursor.fetchone()[0]
+                    session['user'] = name
+                    return redirect(url_for('home_page'))
+                flash("Wrong Password, try again")
+
+            else:
+                flash("Email Not Found, please register")
+                
+    return render_template("login.html",form=form)
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect(url_for('home_page'))
