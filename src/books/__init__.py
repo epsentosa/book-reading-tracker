311d9fabcfa25f28f0707dbcd892131dcd188331
ekoputrasentosa <ekoputrasentosa@gmail.com>
2022-08-19 21:36:48 +0700

    create first API of get request search book title

--- src/books/__init__.py
+++ src/books/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask
 from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 
+app = Flask(__name__)
 bcrypt = Bcrypt()
 mysql = MySQL()
 
 from books.routes import site
+from books.api import api
 
 def create_app(host,user,password,db,secretkey):
-    app = Flask(__name__)
-
     app.config['MYSQL_HOST'] = host
     app.config['MYSQL_USER'] = user
     app.config['MYSQL_PASSWORD'] = password
@@ -18,6 +18,7 @@ def create_app(host,user,password,db,secretkey):
     
     mysql.init_app(app)
     bcrypt.init_app(app)
+    api.init_app(app)
 
     app.register_blueprint(site)
 
